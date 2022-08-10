bd8d8183b63a441ab71e22846cfc736bb3994d79
Mooneous <4087022@naver.com>
2022-08-10 21:09:29 +0900

    state에 담긴 값을 반복처리하면서 가상돔 생성

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,22 +1,34 @@
 import Layout from '../common/Layout';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Department() {
-	const [Members, setMembers] = useState([]);
+	const [Members, setMembers] = useState([]); //초기값은빈배열[]:데이터 배열로 받아옴(axios받아올 state미리 만들어놓음)
 	useEffect(() => {
 		axios
 			.get(process.env.PUBLIC_URL + '/DB/members.json')
 			.then((json) => {
+				//console.log(json.data.members);
 				setMembers(json.data.members);
 			});
 	}, []);
 
-	useEffect(() => {
-		console.log(Members);
-	}, [Members]);
-
-	return <Layout name={'Department'}></Layout>;
+	return (
+		<Layout name={'Department'}>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+						<img
+							src={`${process.env.PUBLIC_URL}/img/${member.pic}`}
+							alt={member.name}
+						/>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Department;
