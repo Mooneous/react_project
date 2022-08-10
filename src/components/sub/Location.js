50e66422dcc34aa8c393805ce0d882a3883ddb2c
Mooneous <4087022@naver.com>
2022-08-11 01:52:06 +0900

    마커 이미지 변경

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -9,17 +9,32 @@ function Location() {
 		level: 3, // 지도의 확대 레벨
 	};
 
-	// 마커가 표시될 위치입니다
-	//var markerPosition = new kakao.maps.LatLng(33.450701,126.570667);
+	// 마커의 위치 정보값
 	const markerPosition = option.center;
-	// 마커를 생성합니다
-	//var marker = new kakao.maps.Marker({position: markerPosition,});
+
+	// 마커의 이미지 정보값(저장위치경로, 이미지크기, 이미지 안에서의 마커 좌표 )
+	const imgSrc =
+		process.env.PUBLIC_URL + '/img/marker1.png';
+	const imgSize = new kakao.maps.Size(232, 99);
+	const imgOpt = {
+		offset: new kakao.maps.Point(116, 99),
+	};
+
+	// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+	const markerImage = new kakao.maps.MarkerImage(
+		imgSrc,
+		imgSize,
+		imgOpt
+	);
+
+	// 위 정보값을 통해 마커생성
 	const marker = new kakao.maps.Marker({
 		position: markerPosition,
+		image: markerImage,
 	});
 
+	//가상돔 요소가 브라우저에 마운트(생성) 되면
 	useEffect(() => {
-		//가상돔 요소가 브라우저에 마운트(생성) 되면
 		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 		//var map = new kakao.maps.Map(mapContainer, mapOption);
 		const map_instance = new kakao.maps.Map(
