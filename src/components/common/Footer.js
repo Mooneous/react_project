022fe2d82fb0dace5738630d38b6b499d9c4582b
Mooneous <4087022@naver.com>
2022-08-10 20:09:42 +0900

    footer 폰트어썸 이미지 폰트 가져오기 및 헤더,푸터,layout에 간단한 스타일링

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faDribbble } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<p>&copy; 2022 LOGO. All Rights Reserved.</p>
+			<ul className='sns'>
+				<li>
+					<FontAwesomeIcon icon={faTwitter} />
+				</li>
+				<li>
+					<FontAwesomeIcon icon={faGithub} />
+				</li>
+				<li>
+					<FontAwesomeIcon icon={faFacebook} />
+				</li>
+
+				<li>
+					<FontAwesomeIcon icon={faDribbble} />
+				</li>
+			</ul>
+		</footer>
+	);
 }
 
 export default Footer;
