88ed4ac5b2f0c24a95b9fde8627a04792dc6116d
Mooneous <4087022@naver.com>
2022-08-10 18:07:02 +0900

    App.js에 컴포넌트 불러오기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+//common
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+//main
+import Content from './components/main/Content';
+import Visual from './components/main/Visual';
+//sub
+import Community from './components/sub/Community';
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+import Location from './components/sub/Location';
+import Members from './components/sub/Members';
+import Youtube from './components/sub/Youtube';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+			<Visual />
+			<Content />
+			<Department />
+			<Community />
+			<Department />
+			<Gallery />
+			<Location />
+			<Members />
+			<Youtube />
+			<Footer />
+		</>
 	);
 }
 
