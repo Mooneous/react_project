99a4b874e3721423fef7c6ac634f3b4a9da9d3be
Mooneous <4087022@naver.com>
2022-08-11 01:21:17 +0900

    지도마커 생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -9,13 +9,26 @@ function Location() {
 		level: 3, // 지도의 확대 레벨
 	};
 
+	// 마커가 표시될 위치입니다
+	//var markerPosition = new kakao.maps.LatLng(33.450701,126.570667);
+	const markerPosition = option.center;
+	// 마커를 생성합니다
+	//var marker = new kakao.maps.Marker({position: markerPosition,});
+	const marker = new kakao.maps.Marker({
+		position: markerPosition,
+	});
+
 	useEffect(() => {
 		//가상돔 요소가 브라우저에 마운트(생성) 되면
 		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 		//var map = new kakao.maps.Map(mapContainer, mapOption);
-		new kakao.maps.Map(container.current, option); //가상돔이니까 container아니고,container.current!
+		const map_instance = new kakao.maps.Map(
+			container.current,
+			option
+		); //가상돔이니까 container아니고,container.current!
 		// 마커가 지도 위에 표시되도록 설정합니다
 		//marker.setMap(map);
+		marker.setMap(map_instance);
 	}, []);
 
 	return (
