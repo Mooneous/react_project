09afaecb69f56f4d63aa53999d62581681d8dda4
Mooneous <4087022@naver.com>
2022-08-10 18:45:01 +0900

    기본 라우터 링크설정(Link/NavLink) 및 활성화스타일객체설정(activeStyle)

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,54 @@
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-	return <div>Header</div>;
+	const active = { color: 'cornflowerblue' };
+
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<nav>
+				<div className='navWrap'>
+					<ul className='gnb'>
+						<li>
+							<NavLink to='/community' activeStyle={active}>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								to='/department'
+								activeStyle={active}>
+								Department
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeStyle={active}>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/location' activeStyle={active}>
+								Location
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/members' activeStyle={active}>
+								Members
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube' activeStyle={active}>
+								Youtube
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
