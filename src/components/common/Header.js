54a6917aa804740221f4bff166a62dc9a6901c20
Mooneous <4087022@naver.com>
2022-08-11 15:00:03 +0900

    header 위치잡기(scss)

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -10,42 +10,42 @@ function Header(props) {
 			</h1>
 
 			<nav>
-				<div className='navWrap'>
-					<ul className='gnb'>
-						<li>
-							<NavLink to='/community' activeStyle={active}>
-								Community
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to='/department'
-								activeStyle={active}>
-								Department
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/gallery' activeStyle={active}>
-								Gallery
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/location' activeStyle={active}>
-								Location
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/members' activeStyle={active}>
-								Members
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/youtube' activeStyle={active}>
-								Youtube
-							</NavLink>
-						</li>
-					</ul>
-				</div>
+				<ul className='gnb gnbL'>
+					<li>
+						<NavLink to='/community' activeStyle={active}>
+							Community
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/department' activeStyle={active}>
+							Department
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeStyle={active}>
+							Gallery
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/location' activeStyle={active}>
+							Location
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/members' activeStyle={active}>
+							Members
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeStyle={active}>
+							Youtube
+						</NavLink>
+					</li>
+				</ul>
+				<ul className='gnb gnbR'>
+					<li>Login</li>
+					<li>Contact</li>
+				</ul>
 			</nav>
 		</header>
 	);
