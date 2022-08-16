bffdf0e6cbb5a4f04610e682e551ab73363c19d7
Mooneous <4087022@naver.com>
2022-08-17 00:18:06 +0900

    체크함수로 인증처리후 에러가 발생시 해당 에러문구가 담긴 객체를 Err스테이트에 옮겨담고 화면에 출력

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -1,11 +1,35 @@
 import Layout from '../common/Layout';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Members() {
 	const initVal = {
 		userid: '',
 	};
 	const [Val, setVal] = useState(initVal);
+
+	//인증 조건 실패시 출력될 에러메시지가 항목별로 담길 state 추가
+	const [Err, setErr] = useState({});
+
+	//인증처리 함수
+	const check = (value) => {
+		const errs = {};
+
+		//userid인증 처리
+		if (value.userid.length < 5) {
+			errs.userid = '아이디를 5글자 이상 입력하세요';
+		}
+		return errs;
+	};
+
+	//submit이벤트 발생시 실행할 함수
+	const handleSubmit = (e) => {
+		//일단은 이벤트의 기본기능을 막아서 서버전송 방지
+		e.preventDefault();
+
+		//check함수에 인수로 val값을 넣어서 인증검사 후
+		//반환된 에러객체값을 Err스테이트에 옮겨담음
+		setErr(check(Val));
+	};
 
 	//현재 입력하고 있는 input값을 state에 저장해주는 함수
 	const handleChange = (e) => {
@@ -18,16 +42,22 @@ function Members() {
 		//console.log(Val, value);
 	};
 
+	useEffect(() => {
+		console.log(Err);
+	}, [Err]); //에러객체가생성되면실행
+
 	return (
 		<Layout name={'Members'}>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<fieldset>
 					<legend>회원가입 폼 양식</legend>
 					<table border='1' width='600'>
 						<caption>회원가입 정보 입력</caption>
 						<tbody>
+							{/* userid */}
 							<tr>
-								<th>
+								<th scope='row'>
+									{/*가로로읽기(접근성)*/}
 									<label htmlFor='userid'>USER ID</label>
 								</th>
 								<td>
@@ -39,11 +69,15 @@ function Members() {
 										value={Val.userid}
 										onChange={handleChange}
 									/>
+									{/*userid 에러메시지*/}
+									<span className='err'>{Err.userid}</span>
 								</td>
 							</tr>
+
 							{/* btnSet */}
 							<tr>
 								<th colSpan='2'>
+									{/*합치기 */}
 									<input type='reset' value='cancel' />
 									<input type='submit' value='submit' />
 								</th>
