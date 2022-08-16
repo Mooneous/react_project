458ff81f7b3e12c491c755b7905fd27e2e40d598
Mooneous <4087022@naver.com>
2022-08-17 01:30:54 +0900

    checkbox 인증처리

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -8,6 +8,7 @@ function Members() {
 		email: '',
 		pwd1: '',
 		pwd2: '',
+		gender: 'null',
 	};
 	const [Val, setVal] = useState(initVal);
 
@@ -45,6 +46,11 @@ function Members() {
 			errs.pwd2 = '두개의 비밀번호를 동일하게 입력하세요';
 		}
 
+		//gender 인증처리
+		if (!value.gender) {
+			errs.gender = '성별을 선택하세요';
+		}
+
 		return errs;
 	};
 
@@ -67,6 +73,13 @@ function Members() {
 		//setVal({...Val, 'userid': value})
 		setVal({ ...Val, [name]: value });
 		//console.log(Val, value);
+	};
+
+	//radio버튼 인풋 전용 함수
+	const handleRadio = (e) => {
+		const { name } = e.target;
+		const isCheck = e.target.checked;
+		setVal({ ...Val, [name]: isCheck });
 	};
 
 	useEffect(() => {
@@ -136,7 +149,7 @@ function Members() {
 									<span className='err'>{Err.pwd1}</span>
 								</td>
 							</tr>
-
+							{/*pwd2 */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='pwd2'>RE-PASSWORD</label>
@@ -151,6 +164,20 @@ function Members() {
 										onChange={handleChange}
 									/>
 									<span className='err'>{Err.pwd2}</span>
+								</td>
+							</tr>
+
+							{/* gender */}
+							<tr>
+								<th scope='row'>GENDER</th>
+								<td>
+									<label htmlFor='male'>MALE</label>
+									<input type='radio' id='male' name='gender' onChange={handleRadio} />
+
+									<label htmlFor='female'>FEMALE</label>
+									<input type='radio' id='female' name='gender' onChange={handleRadio} />
+									{/*name값이 같으므로 둘다체크안됨*/}
+									<span className='err'>{Err.gender}</span>
 								</td>
 							</tr>
 
